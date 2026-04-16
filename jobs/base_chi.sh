557de7c0dea9fe6b8f814a26b104aa81ac8631a7
UFOdestiny <ufodestiny@gmail.com>
2026-04-16 10:47:37 -0400

    v1.1 slurm bash

--- jobs/base_chi.sh
+++ jobs/base_chi.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=Test
+#SBATCH --job-name=chi
 #SBATCH --mail-type=NONE
 #SBATCH --mail-user=dy23a@fsu.edu
 #SBATCH --nodes=1
 #SBATCH --tasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64gb
-#SBATCH --time=96:00:00
+#SBATCH --time=6-23:00:00
 #SBATCH --output=NONE
 #SBATCH --partition=hpg-b200
 #SBATCH --gpus=1
 
-# sbatch jobs/test.sh
-# c; jobs/test.sh
+# sbatch jobs/base_chi.sh
+# c; jobs/base_chi.sh
 
 date
 module load cuda conda
@@ -20,11 +20,11 @@ conda activate st
 
 BASE=/home/dy23a.fsu/st
 SRC=$BASE/src/flow
-PORJ=Test
-LOG=$BASE/output/Test
+PORJ=Chi_Mobi_15min
+LOG=$BASE/output/$PORJ
 mkdir -p $LOG
-ARGS="--bs 1024 --dataset nyc_mobility --proj $PORJ --years 2024 --max_epochs 1"
-# ARGS="--bs 64 --dataset chicago_mobility --proj $PORJ --years 2025 --max_epochs 1"
+# ARGS="--bs 1024 --dataset nyc_mobility --proj $PORJ --years 2024"
+ARGS="--bs 64 --dataset chicago_mobility --proj $PORJ --years 2025"
 
 # MODELS=(
 #     stgcn
